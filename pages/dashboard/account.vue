66a9f250fd894e27136b589adc3ec2f6192bc311
jock <findsource@proton.me>
2026-01-21 10:21:28 +0800

    修复bug

--- pages/dashboard/account.vue
+++ pages/dashboard/account.vue
@@ -528,7 +528,7 @@ const columnDefs = ref<ColDef[]>([
       syncingRowId: syncingRowId,
     },
     cellClass: 'flex justify-center items-center',
-    maxWidth: 130,
+    maxWidth: 100,
     pinned: 'right',
   },
 ]);
